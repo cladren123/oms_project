eac0d7d51a10119a48e5bee75a872acacb571c8d
KimDongEon <gold3987@naver.com>
2024-04-29 23:48:40 +0900

    [feat] OrderConverter 구현

--- monolithic/yogosaza/src/main/java/hyewadong/yogosaza/converter/OrderConverter.java
+++ monolithic/yogosaza/src/main/java/hyewadong/yogosaza/converter/OrderConverter.java
@@ -1,4 +1,19 @@
 package hyewadong.yogosaza.converter;
 
+import hyewadong.yogosaza.dto.order.OrderInputDto;
+import hyewadong.yogosaza.entity.OrderEntity;
+
 public class OrderConverter {
+
+    public static OrderEntity orderInputDtoToOrderEntity(OrderInputDto orderInputDto) {
+        return OrderEntity.builder()
+                .orderSeq(orderInputDto.getOrderSeq())
+                .memberSeq(orderInputDto.getMemberSeq())
+                .itemSeq(orderInputDto.getItemSeq())
+                .orderAddress(orderInputDto.getOrderAddress())
+                .orderQty(orderInputDto.getOrderQty())
+                .orderStatus(orderInputDto.getOrderStatus())
+                .build();
+    }
+
 }
